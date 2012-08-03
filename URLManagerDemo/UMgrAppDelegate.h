2eddeb58e7978c5d3ac0f2736e5aa66a71e6d974
Jiajun <gaosboy@gmail.com>
2012-08-03 17:11:05 +0800

    Add URLManagerDemo proj

---
+++ URLManagerDemo/UMgrAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UMgrAppDelegate.h
+//  URLManagerDemo
+//
+//  Created by jiajun on 8/3/12.
+//  Copyright (c) 2012 jiajun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UMgrAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ URLManagerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  URLManagerDemo
+//
+//  Created by jiajun on 8/3/12.
+//  Copyright (c) 2012 jiajun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UMgrAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UMgrAppDelegate class]));
+    }
+}
