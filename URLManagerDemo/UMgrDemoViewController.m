00d10da2fb417bc0c4477511f4e4aeb635ffa293
Jiajun <gaosboy@gmail.com>
2012-08-09 15:14:59 +0800

    coding hide nav bar in 1st page

--- URLManagerDemo/UMgrDemoViewController.m
+++ URLManagerDemo/UMgrDemoViewController.m
@@ -24,6 +24,18 @@
     return self;
 }
 
+- (void) viewWillAppear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:YES animated:animated];
+    [super viewWillAppear:animated];
+}
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    [super viewWillDisappear:animated];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
